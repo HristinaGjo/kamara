b47f1d2ed981206b1bbd5fb2837a9f29c4a2d728
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-06-21 15:09:39 +0200

    try 1: landing page responsivnes mobile

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../styles/landingPage.module.css"
 import Footer from "../components/Footer";
 
 
 const LandingPage = () => {
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleMouseOver = (index) => {
+    setActiveIndex(index)
+  };
+
+  const handleClick = (index) => {
+    setActiveIndex(index)
+  }
+
+  const titles = [
+    {title:"Echoes", className:classes.titleLink},
+    {title:"Odyssey", className:classes.titleLink},
+    {title:"Cracker", className:classes.titleLink},
+    {title:"Dystopia", className:classes.titleLink},
+  ];
+  
+
   return ( 
     <>
-    <div className={classes.titleItems}>
+    <div className={classes.titleItems} data-active-index={activeIndex}>
       <div className={classes.titleItem}>
-       <div className={classes.titleLink}><h1>Echoes</h1></div>
-        <div className={classes.titleLink}><h1>Odyssey</h1></div>
-        <div className={classes.titleLink}><h1>Cracker</h1></div>
-        <div className={classes.titleLink}><h1>Dystopia</h1></div>
+       {titles.map((item,index) =>(
+        <div
+        key={index}
+        className={item.className}
+        onMouseOver={() => handleMouseOver(index)}
+        onClick={() => handleClick(index)}
+        > 
+        <h1>{item.title}</h1>
+        </div>
+      ))}
     </div>
+    <div className={classes.backgroundPattern}></div>
+    <div className={classes.backgroundImage}></div>
     </div>
-    <Footer/>
     </>
    );
 }
