3830da067eb88546ccfc0a0d3d78c0590aed9fc2
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-06-24 15:30:12 +0200

    write scrollable content from scracth with classes and id's

--- src/pages/Echoes.jsx
+++ src/pages/Echoes.jsx
@@ -4,9 +4,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import img1 from "../assets/echoes-1.jpg"
-import img2 from "../assets/echoes-1.jpg"
-import img3 from "../assets/echoes-1.jpg"
-import img4 from "../assets/echoes-1.jpg"
+import img2 from "../assets/echoes-2.jpg"
+import img3 from "../assets/cracker-1.jpg"
+import img4 from "../assets/cracker-2.jpg"
+import img5 from "../assets/odyssey-1.jpg"
+import img6 from "../assets/odyssey-2.jpg"
+import img7 from "../assets/wasteland-1.jpg"
+import img8 from "../assets/wasteland-2.jpg"
+import img9 from "../assets/odyssey-2.jpg"
 import DraggableScroll from "../components/DraggableScroll";
 
 
@@ -14,8 +19,6 @@ import DraggableScroll from "../components/DraggableScroll";
 const Echoes = () => {
     return ( 
         <>
-
-        <div className={classes.pageCtn}>
 
         <div className={classes.navbarCtn}>
             <div className={classes.logo}>
@@ -31,38 +34,56 @@ const Echoes = () => {
                 <span>contact</span>
                 </Link>
             </div>
-            </div>
-
-        <div className={classes.scrollableContent}>
-            <div className={classes.text}>
-            <h1>Hello</h1>
-            <span>djsnjsnksdmnkmkdmskdmskdmskdmskdmdmsmdmmssmdksmd
-            sdjsidjsdskdmksdmksldmsdmsldmksdmksldmskmdkmskdms
-            sdjsndskjnmdskjmksmksmdksmdkm
-            </span>
         </div>
 
-        <div className={classes.scrollableItem}>
-            <img src={img1}/>
-            </div>
-            <div className={classes.scrollableItem}>
-            <img src={img2}/>
-            </div>
-            <div className={classes.scrollableItem}>
-            <img src={img3}/>
-            </div>
-            <div className={classes.scrollableItem}>
-            <img src={img4}/>
-            </div>
+        <div className={classes.container}>
+            <section className={`${classes.container} ${classes.section01}`}>
+                <h1>Echoes Volume 1.0</h1>
+                <span>sdhshjshdsdnsjndsjdnksdndnds
+                    sndjsndjsndjnsdnskndksndsndd
+                    dnjksndjsndjnsdnjsndjsnjsndjdnnd
+                    sdkssdksmdskdmmsmdsmdsm
+                </span>
+            </section>
+            <section className={`${classes.container} ${classes.section02}`}>
+                <div className={classes.img}>
+                <img className={`${classes.img} ${classes.img01}`} src={img1} alt="image 1"/>
+                <img className={`${classes.img} ${classes.img02}`} src={img2} alt="image 2"/>
+                <img className={`${classes.img} ${classes.img03}`} src={img3} alt="image 3"/>
+                </div>
+            </section>
+            <section className={`${classes.container} ${classes.section03}`}>
+            <div className={classes.img}>
+                    <img className={`${classes.img} ${classes.img04}`} src={img4} alt="image 4"/>
+                    <img className={`${classes.img} ${classes.img05}`} src={img5} alt="image 5"/>
+                    <img className={`${classes.img} ${classes.img06}`} src={img6} alt="image 6"/>
+                </div>
+            </section>
+            <section className={`${classes.container} ${classes.section04}`}>
+            <h1>Echoes sjsjnddnsjn</h1>
+                <span>sdhshjshdsdnsjndsjdnksdndnds
+                    sndjsndjsndjnsdnskndksndsndd
+                    dnjksndjsndjnsdnjsndjsnjsndjdnnd
+                    sdkssdksmdskdmmsmdsmdsm
+                </span>
+            </section>
+
+            <section className={`${classes.container} ${classes.section05}`}>
+            <div className={classes.img}>
+                    <img className={`${classes.img} ${classes.img07}`} src={img7} alt="image 7"/>
+                    <img className={`${classes.img} ${classes.img08}`} src={img8} alt="image 8"/>
+                    <img className={`${classes.img} ${classes.img09}`} src={img9} alt="image 9"/>
+                </div>
+            </section>
         </div>
 
-      { /* <div className={classes.timeline}>
+        <div className={classes.timeline}>
             <div className={classes.scroller}>
-      
+                <span>[<span>Drag</span>]</span>
             </div>
-        </div> */}
-
         </div>
+
+
 
         </>
      );
