0392f0fa7e9779b9fa9a91d053196d8cdf5924cf
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-06-27 13:46:49 +0200

    contact page added, landing page b-img transition modified

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import classes from "../styles/contact.module.css"
+import { Link } from "react-router-dom";
 
-const Contact = () => {
+
+const About = () => {
     return ( 
         <>
-        <h1>Hello</h1>
+        <div className={classes.pageCtn}>
+        <div className={classes.navbarCtn}>
+            <div className={classes.logo}>
+                <Link to="/">
+            <span>[ kamára ]</span>
+            </Link>
+            </div>
+            <div className={classes.navbarRight}>
+                <Link to="/about">
+                <span>[ about ]</span>
+                </Link>
+                <Link to="/contact">
+                <span>[ contact ]</span>
+                </Link>
+            </div>
+        </div>
+
+        <div className={classes.container}>
+
+            <div className={classes.headline}>
+                <h1>Connect<br /></h1>
+                <h2>hello@.kamáranyc.com</h2>
+            </div>
+            <div className={classes.textSpan}>
+                        <span>social</span>
+                        <span>[instagram]</span>
+                        <span>[behance]</span>
+                    </div>
+                    
+        </div>
+        <div className={classes.backgroundImage}> </div>
+        </div>
         </>
      );
 }
  
-export default Contact;
+export default About;
